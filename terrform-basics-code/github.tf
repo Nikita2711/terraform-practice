8b74246a856b13160663cea55068d06bc39bf297
Nikita2711 <72265347+Nikita2711@users.noreply.github.com>
2024-03-01 21:49:17 +0530

    Update github.tf

--- terrform-basics-code/github.tf
+++ terrform-basics-code/github.tf
@@ -9,7 +9,7 @@ terraform {
 
 # Configure the GitHub Provider
 provider "github" {
-    token = "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"
+    token = "****"
 }
 
 resource "github_repository" "example" {
